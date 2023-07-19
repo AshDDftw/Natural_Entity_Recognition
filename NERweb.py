f98ab5dbb5bdf6581411b6714bd309e28685f708
Ashudeep Dubey <94444327+AshDDftw@users.noreply.github.com>
2023-07-19 22:02:53 +0530

    Update NERweb.py

--- NERweb.py
+++ NERweb.py
@@ -3,7 +3,7 @@ import streamlit as st
 from spacy import displacy
 
 # Load the spaCy model
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("NERmodel")
 
 # Set up Streamlit app
 st.title("Data Entity Recognition")
